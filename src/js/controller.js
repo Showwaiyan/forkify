11f5588decfdfb32c32c2154de925ba1964cd8db
Show Wai Yan <showwaiyan555@gmail.com>
2025-02-12 07:34:58 +0800

    Recipe can be now loaded from URL's #id value

--- src/js/controller.js
+++ src/js/controller.js
@@ -47,10 +47,12 @@ const renderSpinner = function(parentEl) {
 
 const showRecipe = async function() {
     try {
+        const id = window.location.hash.slice(1);
+        if (!id) return;
         // Loading recipe
         renderSpinner(recipeContainer);
 
-        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
         const data = await res.json();
 
 
@@ -152,5 +154,6 @@ const showRecipe = async function() {
     } catch (err) {
         alert(err);
     }
-}
-showRecipe();
+};
+
+['hashchange','load'].forEach(ev => window.addEventListener(ev,showRecipe));
