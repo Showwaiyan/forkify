cfa043ff7a2b15e79ae2c57345ecf3a8f54eed87
Show Wai Yan <showwaiyan555@gmail.com>
2025-02-11 12:46:33 +0800

    Implement function that change snake_case property of api data object to camelCase

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,36 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+// For Api data to be fromatted
+function toCamelCase(str) {
+    return str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
+}
+function convertKeysToCamelCaseDeep(obj) {
+    if (Array.isArray(obj)) {
+        return obj.map(convertKeysToCamelCaseDeep);
+    } else if (typeof obj === "object" && obj !== null) {
+        return Object.fromEntries(
+            Object.entries(obj).map(([key, value]) => [
+                toCamelCase(key),
+                convertKeysToCamelCaseDeep(value) // Recursive call
+            ])
+        );
+    }
+    return obj;
+}
+
+
+
+const showRecipe = async function() {
+    try {
+        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+        const data = await res.json();
+        let recipe = convertKeysToCamelCaseDeep(data.data.recipe);
+
+        if (!res.ok) throw new Error(data.message);
+
+    } catch (err) {
+        alert(err);
+    }
+}
+showRecipe();
